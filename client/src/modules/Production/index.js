8a98beb136827c2a2e71b27020f46b7e233bf776
dedmosay <ilinoa90@gmail.com>
2021-03-03 23:00:13 +0300

    add style container fro devices

--- client/src/modules/Production/index.js
+++ client/src/modules/Production/index.js
@@ -4,18 +4,20 @@ import Device from "./Device"
 export default class Production extends React.Component {
 
   render() {
-    
+
     return (
-      <div className="Production">
+      <div className="containerDevices">
         {this.props.devices.map((device, index) => {
           return (
-            <Device
-              key={index}
-              idStyle={index}
-              name={device.name}
-              task={device.task}
-              options={device.options}
-            />
+            <div className={"containerDevices__" + index}>
+              <Device
+                key={index}
+                idStyle={index}
+                name={device.name}
+                task={device.task}
+                options={device.options}
+              />
+            </div>
           )
         })}
       </div>
