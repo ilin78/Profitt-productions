54128c9f6bafe9edf06208afd11475d1f2219bf3
ilinoa <ilinoa90@gmail.com>
2021-03-03 15:44:05 +0300

    add style productions

--- client/src/modules/Production/Device/index.js
+++ client/src/modules/Production/Device/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 
 export default function Device(props) {
- 
   return (
-    <div className="blocks__devices">
-      <div className={"blocks__devices_"+props.idStyle}>
-        <div className="blocks__devices_name">{props.name}</div>
-        <p>{props.task}</p>
-        <p>{props.options}</p>
+    <div className="blocks">
+      <div className="blocks__devices">
+        <div className={"blocks__devices_" + props.idStyle}>
+          <div className="blocks__devices_name">{props.name}</div>
+          <div className="blocks__devices_discription">
+            <div className="blocks__devices_discription-task">{props.task}</div>
+            <div className="blocks__devices_discription-options">{props.options}</div>
+          </div>
+        </div>
       </div>
     </div>
   )
