a9d7a6ba32271e3966b75e542fcb84a17223edc6
ilinoa <ilinoa90@gmail.com>
2021-03-25 17:59:01 +0300

    add filter for solution

--- client/src/modules/Solution/Accessory/index.js
+++ client/src/modules/Solution/Accessory/index.js
@@ -1,9 +1,40 @@
-import React from "react"
+import React from "react";
+import { connect } from "react-redux";
 
-export default function Accessory() {
+import JSONList_devices_ru from "../../../../content/locales/ru/devices_ru.json";
+import JSONList_devices_en from "../../../../content/locales/en/devices_en.json";
+import AccessDevices from "./AccessDevices";
+
+function Accessory({ isLanguage }) {
+  let list_devices = null;
+  if (isLanguage) {
+    list_devices = JSONList_devices_ru.ru[0].list_devices;
+  } else {
+    list_devices = JSONList_devices_en.en[0].list_devices;
+  }
+
   return (
-    <div className="wrapper">
-        <p> Аксессуары </p>
+    <div className="">
+      {list_devices.map((accessory, index) => {
+        if (accessory.div_access === "Есть" || accessory.div_access === "Yes") {
+          return (
+            <div>
+              <AccessDevices
+                key={index}
+                name={accessory.name}
+                discription={accessory.discription}
+              />
+            </div>
+          );
+        }
+      })}
     </div>
-  )
+  );
 }
+ 
+export default connect(
+  (state) => ({
+    isLanguage: state.app.isLanguage,
+  }),
+  null
+)(Accessory);
