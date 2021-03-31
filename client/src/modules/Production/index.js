3cf491c29f2ce66799b55b0f8a98208a6d9b9b30
ilinoa <ilinoa90@gmail.com>
2021-03-31 10:45:36 +0300

    add style for home and add lang

--- client/src/modules/Production/index.js
+++ client/src/modules/Production/index.js
@@ -5,10 +5,9 @@ import { connect } from "react-redux";
 import JSONData_ru from "../../../content/locales/ru/translation_ru.json";
 import JSONData_en from "../../../content/locales/en/translation_en.json";
 
-import './style.scss'
+import "./style.scss";
 
 class Production extends React.Component {
-
   render() {
     let devices = null;
     if (this.props.isLanguage) {
@@ -18,24 +17,26 @@ class Production extends React.Component {
     }
 
     return (
-      <div className="containerDevices">
-        {devices.map((device, index) => {
-          return (
-            <div className={"containerDevices__" + index}>
-              <Device
-                key={index}
-                name={device.name}
-                task={device.task}
-                options={device.options}
-                other={device.other}
-                img_A={device.img_A}
-                img_B={device.img_B}
-                img_C={device.img_C}
-                link={device.link}
-              />
-            </div>
-          );
-        })}
+      <div className="ProductionContainer">
+        <div className="containerDevices">
+          {devices.map((device, index) => {
+            return (
+              <div className={"containerDevices__" + index}>
+                <Device
+                  key={index}
+                  name={device.name}
+                  task={device.task}
+                  options={device.options}
+                  other={device.other}
+                  img_A={device.img_A}
+                  img_B={device.img_B}
+                  img_C={device.img_C}
+                  link={device.link}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
